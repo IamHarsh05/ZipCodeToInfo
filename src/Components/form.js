c6786cad51c0e74b9e2b1716a56bfcf7d8032358
IamHarsh05 <harshchaudhari592@gmail.com>
2023-11-03 19:54:56 +0530

    Update in form.js

--- src/Components/form.js
+++ src/Components/form.js
@@ -10,23 +10,25 @@ export default function Form({ onSubmit }) {
   };
 
   return (
-    <div className="flex flex-col md:flex-row justify-center basis-1/4 items-center py-4 px-8 h-40">
+    <div className="flex flex-col md:flex-row justify-center basis-1/4 items-center mx-4 py-4 px-8 h-40">
       <form
         onSubmit={handleSubmit}
-        className="flex bg-white px-4 py-2 rounded-full"
+        className="flex flex-col md:flex-row justify-center"
       >
-        <button type="submit">
-          <MagnifyingGlassIcon className="h-6 w-6" />
-        </button>
-        <input
-          type="text"
-          id="postalcode"
-          className="bg-transparent outline-none border-b-[3px] mx-2 px-4"
-          placeholder="Enter Postal Code"
-          value={postalcode}
-          onChange={(e) => setPostalcode(e.target.value)}
-        />
-        <div className="mx-2">
+        <div className="flex flex-row justify-center items-center bg-white px-4 py-2 rounded-full">
+          <button type="submit">
+            <MagnifyingGlassIcon className="h-6 w-6" />
+          </button>
+          <input
+            type="text"
+            id="postalcode"
+            className="bg-transparent outline-none border-b-[3px] mx-2 px-4"
+            placeholder="Enter Postal Code"
+            value={postalcode}
+            onChange={(e) => setPostalcode(e.target.value)}
+          />
+        </div>
+        <div className="mx-2 my-2 flex justify-center">
           <button
             type="submit"
             className="py-2 px-2 rounded bg-white text-base"
